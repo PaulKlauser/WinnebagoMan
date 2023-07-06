fdb1162fda153efb9dca85711d1c0c0a93612298
Paul Klauser <paul.klauser@willowtreeapps.com>
2023-07-06 18:57:18 -0400

    Create ViewModel
    Make the app somewhat architected. Don't love the context dependency in there, could abstract a media player interface for testing

--- app/src/main/java/com/paulklauser/winnebagoman/MainActivity.kt
+++ app/src/main/java/com/paulklauser/winnebagoman/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.paulklauser.winnebagoman
 
 import android.annotation.SuppressLint
-import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -32,7 +31,6 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -41,14 +39,13 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.accompanist.insets.ui.TopAppBar
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.paulklauser.winnebagoman.ui.theme.ToolbarGray
 import com.paulklauser.winnebagoman.ui.theme.WinnebagoManTheme
 
 class MainActivity : ComponentActivity() {
-
-    private val mediaPlayers: MutableMap<Int, MediaPlayer> = mutableMapOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -61,38 +58,10 @@ class MainActivity : ComponentActivity() {
                     darkIcons = false
                 )
             }
-            val backingItems = remember {
-                mutableStateListOf(
-                    BackingSoundItem(R.string.acutrama, R.raw.acutrama),
-                    BackingSoundItem(R.string.bathroom, R.raw.bathroom),
-                    BackingSoundItem(R.string.kindness, R.raw.do_me_a_kindness),
-                    BackingSoundItem(R.string.clue_here_now, R.raw.give_a_clue),
-                    BackingSoundItem(R.string.dont_slam, R.raw.dont_slam_the_door),
-                    BackingSoundItem(R.string.fall_off, R.raw.fall_off),
-                    BackingSoundItem(R.string.fly_headlight, R.raw.fly_over_headlight),
-                    BackingSoundItem(R.string.do_it_again, R.raw.do_it_again),
-                    BackingSoundItem(R.string.you_believe, R.raw.believe),
-                    BackingSoundItem(R.string.pth, R.raw.pth),
-                    BackingSoundItem(R.string.the_word_rear, R.raw.rear),
-                    BackingSoundItem(R.string.hehehe, R.raw.hahah),
-                    BackingSoundItem(R.string.make_any_difference, R.raw.make_any_difference),
-                    BackingSoundItem(R.string.dumbass, R.raw.you_dumbass),
-                    BackingSoundItem(R.string.kick, R.raw.kick),
-                    BackingSoundItem(R.string.my_mind, R.raw.mind_shit),
-                    BackingSoundItem(R.string.no_more, R.raw.no_more),
-                    BackingSoundItem(R.string.say_it_right, R.raw.say_it_right),
-                    BackingSoundItem(R.string.shun_of_a, R.raw.shun_of_a_bitch),
-                    BackingSoundItem(R.string.slate_this, R.raw.slate_this),
-                    BackingSoundItem(R.string.what_did_i_say, R.raw.what_did_i_say),
-                    BackingSoundItem(R.string.say_tony, R.raw.what_does_the_line_say_tony),
-                    BackingSoundItem(R.string.flies, R.raw.god_damn_jackass),
-                    BackingSoundItem(R.string.i_dont_give_a, R.raw.i_dont_give_a_shit),
-                    BackingSoundItem(R.string.windshield, R.raw.windshield),
-                    BackingSoundItem(R.string.god_dang_son_of_a, R.raw.god_damn_sonofa)
-                )
-            }
 
-            val displayItems = backingItems.map {
+            val vm = viewModel<SoundBoardViewModel>()
+
+            val displayItems = vm.backingItems.map {
                 soundItemHolder(
                     displayTextRes = it.displayTextRes,
                     audioAssetRes = it.audioAssetRes,
@@ -102,20 +71,8 @@ class MainActivity : ComponentActivity() {
             }
 
             MainScreen(items = displayItems, playAsset = remember {
-                { item ->
-                    mediaPlayers[item.audioAssetRes] =
-                        MediaPlayer.create(this, item.audioAssetRes).apply {
-                            val index =
-                                backingItems.indexOfFirst { it.audioAssetRes == item.audioAssetRes }
-                            backingItems[index] =
-                                backingItems[index].copy(duration = duration, isPlaying = true)
-                            setOnCompletionListener {
-                                release()
-                                backingItems[index] = backingItems[index].copy(isPlaying = false)
-                                mediaPlayers.remove(item.audioAssetRes)
-                            }
-                            start()
-                        }
+                {
+                    vm.playAsset(this, it.audioAssetRes)
                 }
             })
         }
